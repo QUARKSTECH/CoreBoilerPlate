80eb8811a642ec284d7c83d1f13a9f22c81c545a
Mansoor Ahmed <mansooronly4u@gmail.com>
2018-11-04 21:50:38 +0530

    Added Entity and Test projects

---
+++ Test/TestBase/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBase
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
