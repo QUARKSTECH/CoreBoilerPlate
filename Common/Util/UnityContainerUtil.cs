4ca92dd245360680eecde925678e72974844dcfb
Mansoor Ahmed <mansooronly4u@gmail.com>
2018-11-05 00:04:51 +0530

    Added unity package for Dependency Injection.

---
+++ Common/Util/UnityContainerUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity;
+
+namespace Util
+{
+    public class UnityContainerUtil
+    {
+        #region Unity Container
+        private static readonly Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            //RegisterTypes(container);
+            return container;
+        });
+
+        /// <summary>
+        /// Gets the configured Unity container.
+        /// </summary>
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+        #endregion
+    }
+}
